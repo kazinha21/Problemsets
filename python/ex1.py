0a13737fd89e023dcb50cc533238a85f919159b0
kazinha21 <carolinamonteiro21@hotmail.com>
2023-08-31 15:07:23 -0300

    Python1

---
+++ python/ex1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+numero = sys.argv[1]
+numero2 = sys.argv[2]
+numero3 = sys.argv[3]
+nome = sys.argv[4]
+print("Número 1: ", numero, "\nNúmero 2: ", numero2, "\nNúmero 3: ", numero3, "\nMeu nome é: ", nome)
+
---
+++ python/ex1r.py
@@ -0,0 +1,4 @@
+Número 1:  1 
+Número 2:  2 
+Número 3:  3 
+Meu nome é:  Carol
